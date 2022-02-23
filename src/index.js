36b569f77d5351ef48f91c9829e20a193180c1c1
victorschlindwein <victorwilbert@gmail.com>
2022-02-23 18:25:08 -0300

    fourth requiriement done - able to do withdraws only with funds

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const customers = [];
 
 // Middleware
 
-function verifyIfExistsAccountCPF(request, response, next){
+function verifyIfExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers;
 
   const customer = customers.find(customer => customer.cpf === cpf);
@@ -24,7 +24,19 @@ function verifyIfExistsAccountCPF(request, response, next){
   return next();
 }
 
-app.post("/account", (request, response) =>{
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
+app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
   const customerAlreadyExists = customers.some(
@@ -54,7 +66,7 @@ app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer.statement)
 });
 
-app.post("/deposit", verifyIfExistsAccountCPF, (request, response) =>{
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   const { description, amount } = request.body;
 
   const { customer } = request;
@@ -69,6 +81,26 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) =>{
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
-})
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({error: "Insufficient funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
 
 app.listen(3333);
